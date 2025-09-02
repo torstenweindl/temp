e7d90a50bb9c4861fb80eacfb34e6e16f28de1dc
Torsten Weindl <torsten.weindl@gmail.com>
2025-09-02 22:58:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import requests
 import io
 import pandas as pd
 from google.cloud import storage
-from params import *
+# from params import *
 from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 # API_URL = st.secrets["API_URL"]    # API_URL stored in a local "secrets" file; in production, API_URL will be stored in Streamlit's secrets section in the web interface
 # API_URL = 'http://127.0.0.1:8000/segment/'  # API URL hardcoded to the local server for the time being
-API_URL = os.environ.get("API_URL")
+API_URL = st.secrets['API_URL']
 
 file = st.file_uploader("Upload JPG/PNG", type=["png","jpg","jpeg"])
 
