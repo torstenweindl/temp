73df6d24c5db65e89cc8b834420b71a5298da912
Torsten Weindl <torsten.weindl@gmail.com>
2025-09-03 16:37:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ model_list = ['20250826-152119.keras','20250827-141827.keras','20250828-082537.k
 # selected_model = st.selectbox("**Choose the model to be used**" + "\n\n" + "*(list sorted descending by model deployment date - latest model is preselected)*", list_of_blobs)
 # data_to_send = {"option": selected_model}
 
+data_to_send = {"option_1": "no value selected", "option_2": "no value selected", "option_3": "no value selected"}
+
 file = st.file_uploader("Upload JPG/PNG - and this is just a random text", type=["png","jpg","jpeg"])
 
 if file:
@@ -39,7 +41,7 @@ if file:
         buf = io.BytesIO(); image.save(buf, format="JPEG"); buf.seek(0)
 
         try:
-            r = requests.post(API_URL, files={"file": ("image.jpg", buf, "image/jpeg")}, timeout=600)
+            r = requests.post(API_URL, files={"file": ("image.jpg", buf, "image/jpeg")}, data=data_to_send, timeout=600)
             r.raise_for_status()
             data = r.json()
             st.write("This is the test output:")
