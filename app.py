f1f3e3873d04f078df150b0811b106e722439183
Torsten Weindl <torsten.weindl@gmail.com>
2025-09-04 14:59:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ if file:
       if myeloblast_count > 1:
         st.error(f"We found {myeloblast_count} Myeloblast cell(s), which can indicate blood cancer.")
       else:
-        st.write(f"We found no Myeloblast cells, so no indication for blood cancer.")
+        st.write(f"We found no Myeloblast cells, thus no indication for blood cancer.")
 
       st.write(f"#### Cells in detail:")
       
